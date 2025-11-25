ad8111b8961cc7e9c6a56d72ac225d77b9255643
konradkelly <konradky@gmail.com>
2025-11-25 15:27:20 -0800

    implement client-server socket communication

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,19 +1,62 @@
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.Socket;
 // To recompile files and run client:
 // javac src/*.java && java -cp src ChatClient
 
 public class ChatClient {
 
     public static void main(String[] args) {
-       
-    }
+        // try {
+        // readFromFile("LICENSE");       
+        // } catch (IOException e) {
+        //     System.err.println("Error reading from file");
+        //     System.err.println(e.getMessage());
+        //     System.exit(1);
+        // }
+
+        try {
+        readFromSocket("localhost", 12345);       
+        } catch (IOException e) {
+            System.err.println("Error reading from socket");
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }        
+
 
     public static void readFromFile(String filename) throws IOException{
+        InputStream inputStream = new FileInputStream(filename);
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream, java.nio.charset.StandardCharsets.UTF_8);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+        
+        //System.out.println(bufferedReader.readLine());
+        bufferedReader.close();
+
 
     }
 
     public static void readFromSocket(String host, int port) throws IOException {
+        Socket socket = new Socket(host, port);
+
+        InputStream inputstream = socket.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputstream, java.nio.charset.StandardCharsets.UTF_8);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
+        
+        bufferedReader.close();
+        socket.close();
     }
 }
